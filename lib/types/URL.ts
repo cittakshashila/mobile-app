5f2acdaaf0d8e9ea59f2f95e9556320025f9c3ce
RahulNavneeth <rahulmnavneeth@gmail.com>
2024-01-26 01:27:33 +0530

    fix: MEDIA_URL (type)

--- lib/types/URL.ts
+++ lib/types/URL.ts
@@ -1,2 +1,2 @@
 export type INFO_URL = `https://github.com/RahulNavneeth/TK-events-proto/blob/master/${string}/info.json`
-export type MEDIA_URL = `https://github.com/RahulNavneeth/TK-events-proto/blob/master/events/${string}/assets/${string}.jpg`
+export type MEDIA_URL = `https://raw.githubusercontent.com/RahulNavneeth/TK-events-proto/master/events/${string}/assets/${string}.png`
